e715c5c21b71464fe675ee7c9486aae647b2cde8
vmkalima <vmkalima@gmail.com>
2026-08-20 04:51:46 -0400

    Fix typo in expected response body in health handler in main

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
-    if _, err := w.Write([]byte("ok")); err != nil {
+    if _, err := w.Write([]byte("OK")); err != nil {
         log.Printf("Failed to write response: %v", err)
     }
 }
